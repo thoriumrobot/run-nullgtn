43accfe81a94393230b0d87afde91d17e3809a98
thoriumrobot <ks225@njit.edu>
2025-07-08 09:12:06 -0400

    commands

--- mklog.sh
+++ mklog.sh
@@ -2,8 +2,7 @@
 
 cp -R $2/$1 repos/
 
-conda activate naenv
-rm eval.log compile_args/$1.arg.clean
+rm $2_$1.log compile_args/$1.arg.clean
 python eval_repos.py -w -nu $1 > $2_$1.txt
 mv eval.log $2_$1.log
 
