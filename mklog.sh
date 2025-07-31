8b9cd2a57f7d40a5264251dd522da70964dff1d3
thoriumrobot <ks225@njit.edu>
2025-07-31 12:44:52 -0400

    fix script

--- mklog.sh
+++ mklog.sh
@@ -3,7 +3,7 @@
 python eval_repos.py -w -nu $1
 rm eval.log
 cp -R $2/$1 repos/
-cp -R gradle/$1/* repos/$1/*
+cp -R gradle/$1 repos/
 
 rm $2_$1.log compile_args/$1.arg.clean
 python eval_repos.py -w -nu $1 > $2_$1.txt
